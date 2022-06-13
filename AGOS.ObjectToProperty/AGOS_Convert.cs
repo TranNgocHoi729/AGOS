6d24582b25f726fdba1f2a9979a0384e91ff9cf9
lmha@cmc.com.vn <lmha@cmc.com.vn>
2022-06-13 13:16:01 +0700

    Create AGOS lib

---
+++ AGOS.ObjectToProperty/AGOS_Convert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGOS.ObjectToProperty
+{
+    public interface AGOS_Convert
+    {
+        public List<Item> ConvertObjectToListProperty(object input);
+
+        public List<Item> ConvertObjectToListProperty(object input, List<string> SelectedFields);
+
+        public List<Item> ConvertObjectToListPropertyExcept(object input, List<string> Excepts);
+
+    }
+}
---
+++ AGOS.ObjectToProperty/AGOS_Convert_Handle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGOS.ObjectToProperty
+{
+    public class AGOS_Convert_Handle : AGOS_Convert
+    {
+        public List<Item> ConvertObjectToListProperty(object input)
+        {
+            List<Item> Items = new List<Item>();
+            var props = input.GetType().GetProperties();
+            foreach (var item in props)
+            {
+                Items.Add(new Item
+                {
+                    Key = item.Name,
+                    Value = item.GetValue(input)
+                });
+            }
+            return Items;
+        }
+
+        public List<Item> ConvertObjectToListProperty(object input, List<string> SelectedFields)
+        {
+            List<Item> Items = new List<Item>();
+            var props = input.GetType().GetProperties();
+            foreach (var item in props)
+            {
+                foreach (var select in SelectedFields)
+                {
+                    if (item.Name.Equals(select))
+                    {
+                        Items.Add(new Item
+                        {
+                            Key = item.Name,
+                            Value = item.GetValue(input)
+                        });
+                    }
+                }
+            }
+            return Items;
+        }
+
+        public List<Item> ConvertObjectToListPropertyExcept(object input, List<string> Excepts)
+        {
+            List<Item> Items = new List<Item>();
+            var props = input.GetType().GetProperties();
+            foreach (var item in props)
+            {
+                foreach (var select in Excepts)
+                {
+                    if (!item.Name.Equals(select))
+                    {
+                        Items.Add(new Item
+                        {
+                            Key = item.Name,
+                            Value = item.GetValue(input)
+                        });
+                    }
+                }
+            }
+            return Items;
+        }
+    }
+}
